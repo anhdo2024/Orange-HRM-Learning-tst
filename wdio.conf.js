8f166c547d3c96e88a2e2d06b2595ef5916f7a3f
anhdo2024 <anhdn311082@gmail.com>
2026-07-30 22:37:21 +0700

    Testing With Fail TestCase AND UPLOAD IMAGE FAIL

--- wdio.conf.js
+++ wdio.conf.js
@@ -245,11 +245,12 @@ export const config = {
      * @param {boolean} result.passed    true if test has passed, otherwise false
      * @param {object}  result.retries   information about spec related retries, e.g. `{ attempts: 0, limit: 0 }`
      */
-    afterTest: async function (test, context, { error, result, duration, passed, retries }) {
+    //  afterTest: async function (test, context, { error, result, duration, passed, retries }) {
+    afterTest: async function (test, context, { passed }) {
         if (!passed) {
             //await browser.takeScreenshot();
             // Lưu vào thư mục screenshots + đính vào Allure
-            await browser.saveScreenshot(`./screenshots/${test.title}.png`);
+            await browser.saveScreenshot(`./screenshots/${test.title.replace(/\s+/g, '_')}.png`);
         }
     },
 
